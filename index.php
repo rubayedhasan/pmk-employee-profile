06180e39290dc794aca8dc7c1b2ec60d3878f189
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-05-22 12:22:31 +0600

    adding basic file folder and links

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Profile</title>
+    <!-- Linked favicon  -->
+    <link rel="shortcut icon" href="./assets/images/PMK_ERP_Logo.png" type="image/x-icon">
+
+    <!-- Linked custom stylesheet -->
+    <link rel="stylesheet" href="./styles/profile.css">
+</head>
+
+<body>
+    <main class="container-width">
+        <p>Profile</p>
+    </main>
+
+
+    <!-- Linked custom script -->
+    <script src="./js/profile.js"></script>
+</body>
+
+</html>
