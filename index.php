05a29b6f9374fbd0bdd2f57eed2fc0f3c7aeba3b
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-05-23 18:04:28 +0600

    adding profile-main section's all sub-section's structure

--- index.php
+++ index.php
@@ -249,6 +249,115 @@
                         </button>
                     </div>
                 </div>
+
+                <!-- employment information  -->
+                <div class="employment-info-container">
+
+                    <!-- employee contract details  -->
+                    <div class="employee-contract-layout">
+                        <!-- layout heading  -->
+                        <div class="layout-heading">
+                            <div class="lh-label-icon">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-contract">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M8 21h-2a3 3 0 0 1 -3 -3v-1h5.5" />
+                                    <path d="M17 8.5v-3.5a2 2 0 1 1 2 2h-2" />
+                                    <path d="M19 3h-11a3 3 0 0 0 -3 3v11" />
+                                    <path d="M9 7h4" />
+                                    <path d="M9 11h4" />
+                                    <path d="M18.42 12.61a2.1 2.1 0 0 1 2.97 2.97l-6.39 6.42h-3v-3l6.42 -6.39" />
+                                </svg>
+                            </div>
+                            <h4 class="lh-label">Contract Info</h4>
+                        </div>
+                    </div>
+
+                    <!-- employee Compensation details  -->
+                    <div class="employee-compensation-layout">
+                        <!-- layout heading  -->
+                        <div class="layout-heading">
+                            <div class="lh-label-icon">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-currency-taka">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M15.5 15.5a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                                    <path d="M7 7a2 2 0 1 1 4 0v9a3 3 0 0 0 6 0v-.5" />
+                                    <path d="M8 11h6" />
+                                </svg>
+                            </div>
+                            <h4 class="lh-label">Compensation</h4>
+                        </div>
+
+                    </div>
+                </div>
+
+                <!-- employee transfer details  -->
+                <div class="employee-transfer-details-container">
+                    <!-- layout heading  -->
+                    <div class="layout-heading">
+                        <div class="lh-label-icon">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-transfer">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M20 10h-16l5.5 -6" />
+                                <path d="M4 14h16l-5.5 6" />
+                            </svg>
+                        </div>
+                        <h4 class="lh-label">Transfer Log</h4>
+                    </div>
+                </div>
+
+                <!-- employee promotion details  -->
+                <div class="employee-promotion-details-container">
+                    <!-- layout heading  -->
+                    <div class="layout-heading">
+                        <div class="lh-label-icon">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-sort-ascending-2">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M14 9l3 -3l3 3" />
+                                <path d="M5 5.5a.5 .5 0 0 1 .5 -.5h4a.5 .5 0 0 1 .5 .5v4a.5 .5 0 0 1 -.5 .5h-4a.5 .5 0 0 1 -.5 -.5l0 -4" />
+                                <path d="M5 14.5a.5 .5 0 0 1 .5 -.5h4a.5 .5 0 0 1 .5 .5v4a.5 .5 0 0 1 -.5 .5h-4a.5 .5 0 0 1 -.5 -.5l0 -4" />
+                                <path d="M17 6v12" />
+                            </svg>
+                        </div>
+                        <h4 class="lh-label">Promotion History</h4>
+                    </div>
+                </div>
+
+                <!-- employee increment details  -->
+                <div class="employee-increment-details-container">
+                    <!-- layout heading  -->
+                    <div class="layout-heading">
+                        <div class="lh-label-icon">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cash-banknote-plus">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                                <path d="M12.25 18h-7.25a2 2 0 0 1 -2 -2v-8a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v4.5" />
+                                <path d="M18 12h.01" />
+                                <path d="M6 12h.01" />
+                                <path d="M16 19h6" />
+                                <path d="M19 16v6" />
+                            </svg>
+                        </div>
+                        <h4 class="lh-label">Increment History</h4>
+                    </div>
+                </div>
+
+                <!-- employee Disciplinary Action  -->
+                <div class="employee-disciplinary-action-container">
+                    <!-- layout heading  -->
+                    <div class="layout-heading">
+                        <div class="lh-label-icon">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user-exclamation">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4c.348 0 .686 .045 1.008 .128" />
+                                <path d="M19 16v3" />
+                                <path d="M19 22v.01" />
+                            </svg>
+                        </div>
+                        <h4 class="lh-label">Disciplinary Action Register
+                        </h4>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
